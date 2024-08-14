e13db1fb278c434ecbf74e85eae752c5ae50eaf4
Aachalyeole <aachalyeole20@gmail.com>
2024-08-14 13:05:41 +0530

    jobseekeradded

--- src/main/java/com/example/JobPortalbatch1/model/JobSeekerProfile.java
+++ src/main/java/com/example/JobPortalbatch1/model/JobSeekerProfile.java
@@ -1,4 +1,31 @@
 package com.example.JobPortalbatch1.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class JobSeekerProfile {
+
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private long id;
+        private String resume;
+        private String url;
+        private String certifications;
+        private String Skills;
+        private User user;
+        private Set<Job> appliedJobs;
+
+
+
 }
