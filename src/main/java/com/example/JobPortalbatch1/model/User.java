8883326e991bd020345c0ed2a15d41c93179838b
SurajNgm <surajngm9076@gmail.com>
2024-08-14 12:49:25 +0530

    usercreated2

--- src/main/java/com/example/JobPortalbatch1/model/User.java
+++ src/main/java/com/example/JobPortalbatch1/model/User.java
@@ -1,4 +1,30 @@
 package com.example.JobPortalbatch1.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+    private String Password;
+    private String role;
+    private int contactNumber;
+    private EmployerProfile employerProfile;
+    private AdminProfile adminProfile;
+    private JobSeekerProfile jobSeekerProfile;
 }
